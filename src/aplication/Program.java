49906ecf354f7979931a3d4d5910a9f7655686c5
Alef Bispo <alef.b.dos.santos@hotmail.com>
2020-12-14 20:17:37 -0300

    Finalizando

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,6 +1,10 @@
 package aplication;
 
 import aplication.entites.Client;
+import aplication.entites.Order;
+import aplication.entites.OrderItem;
+import aplication.entites.Product;
+import entites_enum.OrderStatus;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -11,6 +15,7 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) throws ParseException {
+
         Scanner scanner = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         System.out.println("Enter cliente data: ");
@@ -21,12 +26,39 @@ public class Program {
        String email=scanner.nextLine();
         System.out.print("Birth date (DD/MM/YYYY): ");
         Date birthDate = sdf.parse(scanner.next());
+
         Client client = new Client(name,email,birthDate);
 
         System.out.println();
         System.out.println("Enter order data: ");
         System.out.print("Status: ");
+        OrderStatus status = OrderStatus.valueOf(scanner.next());
 
+        Order order = new Order(new Date(), status, client);
+
+        System.out.print("How many items to this order? ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Enter #" + i + " item data:");
+            System.out.print("Product name: ");
+            scanner.nextLine();
+            String produtcName = scanner.nextLine();
+            System.out.print("Product price: ");
+            double productPrice = scanner.nextDouble();
+
+            Product product = new Product(produtcName,productPrice);
+
+            System.out.print("Quantity: ");
+            int quantity = scanner.nextInt();
+
+            OrderItem orderItem = new OrderItem(quantity,productPrice,product);
+
+            order.addItem(orderItem);
+        }
+
+        System.out.println();
+        System.out.println("ORDER SUMMARY: ");
+        System.out.println(order);
 
 
 
--- src/aplication/entites/Client.java
+++ src/aplication/entites/Client.java
@@ -1,13 +1,16 @@
 package aplication.entites;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Client {
+
+    private static final SimpleDateFormat sdf =
+            new SimpleDateFormat("dd/MM/yyyy");
+
     private String name;
     private String email;
     private Date birthDate;
-
-    public Client(){}
 
     public Client(String name, String email, Date birthDate) {
         this.name = name;
@@ -37,5 +40,9 @@ public class Client {
 
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
+    }
+    @Override
+    public String toString() {
+        return name + " (" + sdf.format(birthDate) + ") - " + email;
     }
 }
